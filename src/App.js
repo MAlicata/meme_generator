230d689b85b884b3ccdc584c857135dfc9f64f43
MAlicata <matiasalicata@gmail.com>
2022-12-18 01:17:59 -0300

    add images

--- src/App.js
+++ src/App.js
@@ -6,16 +6,26 @@ function App() {
 
   const [linea1, setLinea1] = useState("");
   const [linea2, setLinea2] = useState("");
+  const [imagen, setImagen] = useState("");
+
   const onChangeLinea1 = function(evento) {
     setLinea1(evento.target.value)
   }
+
   const onChangeLinea2 = function(evento) {
     setLinea2(evento.target.value)
   }
+
+  const onChangeImagen = function(evento) {
+    setImagen(evento.target.value)
+  }
+  const onClickExportar = function(evento) {
+    alert("EXPORTAR")
+  }
   return (
     <div className="App">
-     <select>
-      <option value="casa">Casa en llamas</option>
+     <select onChange={onChangeImagen}>
+      <option value="fire">Casa en llamas</option>
       <option value="futurama">Futurama</option>
       <option value="history">History Channel</option>
       <option value="matrix">Matrix</option>
@@ -25,12 +35,12 @@ function App() {
 
      <input onChange={onChangeLinea1} type="text" placeholder="Linea 1"/><br/>
      <input onChange={onChangeLinea2} type="text" placeholder="Linea 2"/><br/>
-     <button>Exportar</button>
+     <button onClick={onClickExportar}>Exportar</button>
      
-     <div>
+     <div className="meme">
       <span>{linea1}</span><br />
       <span>{linea2}</span>
-      <img src="" />
+      <img src={"/img/"+ imagen + ".jpg"}/>
      </div>
 
     </div>
